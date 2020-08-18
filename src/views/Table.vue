9dc8dbd2bd1d53a7f2501dadb9e5b26dc2b01b76
salsabilamrzkaa <salsabila.maurizka@amartha.com>
2020-08-18 10:28:26 +0700

    styling table

--- src/views/Table.vue
+++ src/views/Table.vue
@@ -1,154 +1,123 @@
 <template>
-  <b-container fluid>
-    <!-- User Interface controls -->
-    <b-row>
-      <b-col lg="6" class="my-1">
-        <b-form-group
-          label="Sort"
-          label-cols-sm="3"
-          label-align-sm="right"
-          label-size="sm"
-          label-for="sortBySelect"
-          class="mb-0"
-        >
-          <b-input-group size="sm">
-            <b-form-select v-model="sortBy" id="sortBySelect" :options="sortOptions" class="w-75">
-              <template v-slot:first>
-                <option value="">-- none --</option>
-              </template>
-            </b-form-select>
-            <b-form-select v-model="sortDesc" size="sm" :disabled="!sortBy" class="w-25">
-              <option :value="false">Asc</option>
-              <option :value="true">Desc</option>
-            </b-form-select>
-          </b-input-group>
-        </b-form-group>
-      </b-col>
+  <div class="container">
+      <b-container fluid>
+        <!-- User Interface controls -->
+        <b-row>
+          <b-col lg="6" class="my-1">
+            <b-form-group
+              label="Filter"
+              label-cols-sm="2"
+              label-size="sm"
+              label-for="filterInput"
+              class="mb-0"
+              style="margin-left: 10px;"
+            >
+              <b-input-group size="sm">
+                <b-form-input
+                  v-model="filter"
+                  type="search"
+                  id="filterInput"
+                  placeholder="Type to Search"
+                ></b-form-input>
+                <b-input-group-append>
+                  <b-button :disabled="!filter" @click="filter = ''" style="background-color: #3CA59D; border-color: #3CA59D;">Clear</b-button>
+                </b-input-group-append>
+              </b-input-group>
+            </b-form-group>
+          </b-col>
+          
+          <b-col lg="3" md="0" class="my-1"></b-col>
 
-      <b-col lg="6" class="my-1">
-        <b-form-group
-          label="Initial sort"
-          label-cols-sm="3"
-          label-align-sm="right"
-          label-size="sm"
-          label-for="initialSortSelect"
-          class="mb-0"
-        >
-          <b-form-select
-            v-model="sortDirection"
-            id="initialSortSelect"
-            size="sm"
-            :options="['asc', 'desc', 'last']"
-          ></b-form-select>
-        </b-form-group>
-      </b-col>
+          <b-col lg="3" class="my-1">
+              <b-form-group
+                label="Per page"
+                label-cols-sm="6"
+                label-cols-md="2"
+                label-cols-lg="3"
+                label-align-sm="left"
+                label-size="sm"
+                label-for="perPageSelect"
+                class="mb-0"
+              >
+              <b-form-select
+                v-model="perPage"
+                id="perPageSelect"
+                size="sm"
+                :options="pageOptions"
+              ></b-form-select>
+            </b-form-group>
+          </b-col>
 
-      <b-col lg="6" class="my-1">
-        <b-form-group
-          label="Filter"
-          label-cols-sm="3"
-          label-align-sm="right"
-          label-size="sm"
-          label-for="filterInput"
-          class="mb-0"
-        >
-          <b-input-group size="sm">
-            <b-form-input
-              v-model="filter"
-              type="search"
-              id="filterInput"
-              placeholder="Type to Search"
-            ></b-form-input>
-            <b-input-group-append>
-              <b-button :disabled="!filter" @click="filter = ''">Clear</b-button>
-            </b-input-group-append>
-          </b-input-group>
-        </b-form-group>
-      </b-col>
+        <!-- Main table element -->
+        <div style="width:97%">
+            <b-table
+                show-empty
+                small
+                stacked="md"
+                :items="users"
+                :fields="fields"
+                :current-page="currentPage"
+                :per-page="perPage"
+                :filter="filter"
+                :filterIncludedFields="filterOn"
+                :sort-by.sync="sortBy"
+                :sort-desc.sync="sortDesc"
+                :sort-direction="sortDirection"
+                @filtered="onFiltered"
+              >
 
-      <b-col sm="5" md="6" class="my-1">
-        <b-form-group
-          label="Per page"
-          label-cols-sm="6"
-          label-cols-md="4"
-          label-cols-lg="3"
-          label-align-sm="right"
-          label-size="sm"
-          label-for="perPageSelect"
-          class="mb-0"
-        >
-          <b-form-select
-            v-model="perPage"
-            id="perPageSelect"
-            size="sm"
-            :options="pageOptions"
-          ></b-form-select>
-        </b-form-group>
-      </b-col>
+                <template v-slot:cell(name)="row">
+                  {{ row.value.first }} {{ row.value.last }}
+                </template>
 
-    <!-- Main table element -->
-    <b-table
-      show-empty
-      small
-      stacked="md"
-      :items="users"
-      :fields="fields"
-      :current-page="currentPage"
-      :per-page="perPage"
-      :filter="filter"
-      :filterIncludedFields="filterOn"
-      :sort-by.sync="sortBy"
-      :sort-desc.sync="sortDesc"
-      :sort-direction="sortDirection"
-      @filtered="onFiltered"
-    >
-      <!-- <template v-slot:cell(avatar)="row">
-        <img :src="{{row.avatar}}" alt="" />
-      </template> -->
+                <template v-slot:cell(actions)="row">
+                  <b-button size="sm" @click="info(row.item, row.index, $event.target)" class="mr-1">
+                    Info modal
+                  </b-button>
+                  <b-button size="sm" @click="row.toggleDetails">
+                    {{ row.detailsShowing ? 'Hide' : 'Show' }} Details
+                  </b-button>
+                </template>
 
-      <template v-slot:cell(name)="row">
-        {{ row.value.first }} {{ row.value.last }}
-      </template>
+                <template v-slot:row-details="row">
+                  <b-card>
+                    <ul>
+                      <li v-for="(value, key) in row.item" :key="key">{{ key }}: {{ value }}</li>
+                    </ul>
+                  </b-card>
+                </template>
+              </b-table>
+        </div>
+        </b-row>
 
-      <template v-slot:cell(actions)="row">
-        <b-button size="sm" @click="info(row.item, row.index, $event.target)" class="mr-1">
-          Info modal
-        </b-button>
-        <b-button size="sm" @click="row.toggleDetails">
-          {{ row.detailsShowing ? 'Hide' : 'Show' }} Details
-        </b-button>
-      </template>
+        <b-row>
+            <b-col>
+                <div style="margin: 30px 0 0 0; display: flex; justify-content: space-between; align-items: center;">
+                        <b-tfoot class="total-row">
+                          <b-tr>
+                            <b-td colspan="7" class="text-right">
+                              Total Rows: <b>{{users.length}}</b>
+                            </b-td>
+                          </b-tr>
+                        </b-tfoot>
 
-      <template v-slot:row-details="row">
-        <b-card>
-          <ul>
-            <li v-for="(value, key) in row.item" :key="key">{{ key }}: {{ value }}</li>
-          </ul>
-        </b-card>
-      </template>
-    </b-table>
-    </b-row>
-
-    <b-row>
-        <b-col>
-            <div style="margin: 10px 0 0 0;">
-                    <b-pagination
-                    v-model="currentPage"
-                    :total-rows="totalRows"
-                    :per-page="perPage"
-                    align="center"
-                    size="md"
-                    class="my-1"
-                    style="margin-left: 0;"
-                ></b-pagination>
-            </div>
-        </b-col>
-    </b-row>
-    <!-- Info modal -->
-    <b-modal :id="infoModal.id" :title="infoModal.title" ok-only @hide="resetInfoModal">
-      <pre>{{ infoModal.content }}</pre>
-    </b-modal>
-  </b-container>
+                        <!-- <b-pagination
+                          v-model="currentPage"
+                          :total-rows="totalRows"
+                          :per-page="perPage"
+                          align="fill"
+                          size="sm"
+                          class="my-0"
+                        ></b-pagination> -->
+                </div>
+            </b-col>
+        </b-row>
+        <!-- Info modal -->
+        <b-modal :id="infoModal.id" :title="infoModal.title" ok-only @hide="resetInfoModal">
+          <pre>{{ infoModal.content }}</pre>
+        </b-modal>
+      </b-container>
+  </div>
 </template>
 
 <script>
@@ -159,7 +128,6 @@ export default {
       return {
         users: [],
         fields: [
-          { key: 'avatar', label: 'Avatar', sortable: false},
           { key: 'first_name', label: 'First name', sortable: true, sortDirection: 'desc' },
           { key: 'last_name', label: 'Last name', sortable: true, sortDirection: 'desc' },
           { key: 'email', label: 'Email', sortable: true, sortDirection: 'desc'},
@@ -167,7 +135,7 @@ export default {
         totalRows: 1,
         currentPage: 1,
         perPage: 5,
-        pageOptions: [5, 10, 15],
+        pageOptions: [3, 5, 10, 20],
         sortBy: '',
         sortDesc: false,
         sortDirection: 'asc',
@@ -223,5 +191,26 @@ export default {
 <style>
 .container-fluid{
   margin-top: 50px;
+}
+.table{
+  width: 97%;
+  margin-left: 20px;
+  margin-top: 1.5rem;
+}
+.mb-1, .my-1 {
+margin-bottom: 1.25rem !important;
+}
+.pagination .page-item.active .page-link{
+  background-color: #3CA59D;
+  border-color: #3CA59D;
+}
+.table > thead > tr > th {
+  padding: 10px;
+}
+.table-sm th, .table-sm td {
+  padding: 0.7rem !important;
+}
+.total-row{
+  margin-left: 10px;
 }
 </style>
